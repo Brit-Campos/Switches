567d9d55bc04150cfec92b7dc253f8bf6760feec
Brittanie Campos <bri2212040@maricopa.edu>
2022-06-22 12:21:11 -0700

    Add project files.

---
+++ Switches/Program.cs
@@ -0,0 +1,34 @@
+﻿// switch = an efficient alternative to many else if statements
+
+Console.WriteLine("What day is it today?");
+String day = Console.ReadLine();
+
+switch(day)
+{
+    case "Monday":
+        Console.WriteLine("It's Monday!");
+        break;
+    case "Tuesday":
+        Console.WriteLine("It's Monday!");
+        break;
+    case "Wednesday":
+        Console.WriteLine("It's Monday!");
+        break;
+    case "Thursday":
+        Console.WriteLine("It's Monday!");
+        break;
+    case "Friday":
+        Console.WriteLine("It's Monday!");
+        break;
+    case "Saturday":
+        Console.WriteLine("It's Monday!");
+        break;
+    case "Sunday":
+        Console.WriteLine("It's Monday!");
+        break;
+    default:
+        Console.WriteLine(day + "is not a day!");
+        break;
+}
+
+Console.ReadKey();
